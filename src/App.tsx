ee636eaddabb0a6d36c07b24c61afc587c1d3fd6
Italo Ferreira <grd788@gmail.com>
2022-07-18 15:22:01 -0500

    feat :sparkles: init modifications and react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,59 +1,15 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { User } from './components/User'
 import GlobalStyles from './styles/globalStyles'
-import img from './assets/logo-github.png'
-import { useState } from 'react'
-import { Header } from './components/Header'
-import { Profile } from './components/Profile'
-import { Repository } from './components/Repository'
 
 function App() {
-  function handleClick() {
-    const user = fetch(`https://api.github.com/users/${input}`)
-    .then(response => response.json())
-    .then(json => setuserData(json))
-
-    return user
-  }
-
-  const [input, setInput] = useState('');
-  const [userData, setuserData] = useState<any>(null);
-
-  localStorage.setItem('user', input);
-
   return (
-    <div>
+    <BrowserRouter>
       <GlobalStyles />
-      {!userData ? (
-        <div className="container">
-          <img src={img} alt="" />
-          <input
-            value={input}
-            onChange={event => {
-              setInput(event.currentTarget.value)
-            }}
-            type="text"
-            placeholder="Enter user name"
-          />
-          <button onClick={handleClick} type="submit">
-            Search
-          </button>
-        </div>
-      ) : (
-        <>
-          <Header />
-          <Profile 
-          name={userData.name}
-          avatar_url={userData.avatar_url} 
-          login={userData.login} 
-          location={userData.location}
-          company={userData.company}
-          followers={userData.followers}
-          following={userData.following}
-          public_repos={userData.public_repos}
-          />
-          <Repository/>
-        </>
-      )}
-    </div>
+      <Routes>
+        <Route path="/" element={<User />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
